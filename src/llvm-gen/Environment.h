fd3a7d2b7b9a6f38d76cb03e3ca8251719e61ff2
Bug Digger <marko@bugdigger.com>
2024-05-05 11:54:53 +0200

    setting up namespace and context

--- src/llvm-gen/Environment.h
+++ src/llvm-gen/Environment.h
@@ -5,26 +5,27 @@
 #include <unordered_map>
 #include <map>
 
-#include "llvm/IR/Value.h"
+#include <llvm/ADT/DenseMap.h>
 
+template <typename K, typename V>
 class Environment {
 public:
-    std::shared_ptr<Environment> m_Enclosing;
-    std::unordered_map<std::string, llvm::Value*> m_Values;
+    std::shared_ptr<Environment<K, V> > m_Enclosing;
+    llvm::DenseMap<K, V> m_Values;
 public:
     Environment() = default;
 
     explicit Environment(std::shared_ptr<Environment> enclosing);
 
     // Use the Token overload because it can then report errors using the token's line. Only use the string overload when there's no token.
-    void define(const std::string& identifier, llvm::Value* value);
+    void define(K& identifier, V* value);
 
     // `lookup()` can be renamed to `get()`
-    llvm::Value* lookup(const std::string& identifier);
-    llvm::Value* getAt(int distance, const std::string& identifier);
+    V* lookup(K& identifier);
+    V* getAt(int distance, K& identifier);
 
-    void assign(const std::string& identifier, llvm::Value* value);
-    void assignAt(int distance, const std::string& identifier, llvm::Value* value);
+    void assign(K& identifier, V* value);
+    void assignAt(int distance, K& identifier, V* value);
 
     Environment* ancestor(int distance);
 };
--- src/llvm-gen/KarolaScriptNamespace.h
+++ src/llvm-gen/KarolaScriptNamespace.h
@@ -1,8 +1,57 @@
-//
-// Created by marko on 5/4/24.
-//
+#pragma once
 
-#ifndef KAROLASCRIPT_KAROLASCRIPTNAMESPACE_H
-#define KAROLASCRIPT_KAROLASCRIPTNAMESPACE_H
+#include <llvm/ADT/SmallString.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/IR/Module.h>
+#include <llvm/ADT/Optional.h>
 
-#endif //KAROLASCRIPT_KAROLASCRIPTNAMESPACE_H
+#include <mlir/IR/Builders.h>
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/OwningOpRef.h>
+#include <mlir/IR/Value.h>
+#include <mlir/Support/LogicalResult.h>
+
+#include "KarolaScriptContext.h"
+#include "Environment.h"
+#include "../util/common.h"
+
+/// KarolaScript's namespaces are the unit of compilation.
+/// Any code that needs to be compiled has to be in a namespace.
+/// The official way to create a new namespace is to use the `makeNamespace` function.
+class KarolaScriptNamespace {
+private:
+    KarolaScriptContext& ctx;
+
+    mlir::StringRef name;
+    llvm::Optional<std::string> filename;
+
+    /// The root environment of the namespace on the semantic analysis phase.
+    /// Which is a mapping from names to AST nodes ( no evaluation ).
+    Environment<std::string, UniqueExprPtr> semanticEnv;
+
+    /// Th root environment to store the MLIR value during the IR generation phase.
+    Environment<llvm::StringRef, mlir::Value> symbolTable;
+
+public:
+    KarolaScriptNamespace(KarolaScriptContext &ctx, llvm::StringRef ns_name,
+            llvm::Optional<llvm::StringRef> filename);
+
+    KarolaScriptContext &getContext();
+
+    /// Generate and return a MLIR ModuleOp tha contains the IR of the namespace
+    /// with respect to the compilation phase
+    mlir::OwningOpRef<mlir::ModuleOp> generate();
+
+    /// Compile the namespace to a llvm module.
+    /// It will call the `generate` method of the namespace to generate the IR.
+    std::unique_ptr<llvm::Module> compileToLLVM();
+
+    /// Run all the passes specified in the context on the given MLIR ModuleOp.
+    mlir::LogicalResult runPasses(mlir::ModuleOp &m);
+
+    /// Dumps the namespace with respect to the compilation phase
+    void dump();
+
+    ~KarolaScriptNamespace();
+
+};
