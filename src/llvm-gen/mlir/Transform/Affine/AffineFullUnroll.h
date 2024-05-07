103df9414f69f41c7bc7f1b91e97d317159a87ee
Bug Digger <marko@bugdigger.com>
2024-05-07 14:35:48 +0200

    an include/ directory for headers and tablegen files (more on tablegen in a future), and a lib/ directory for implementation code. Then, within those two directories a project would have a Transform/ subdirectory that stores the files for passes that transform code within a dialect, Conversion/ for passes that convert between dialects, Analysis/ for analysis passes, etc. Each of these directories might have subdirectories for the specific dialects they operate on.

---
+++ src/llvm-gen/mlir/Transform/Affine/AffineFullUnroll.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Pass/Pass.h"
+
+namespace mlir {
+    namespace tutorial {
+
+        class AffineFullUnrollPass
+                : public PassWrapper<AffineFullUnrollPass,
+                        OperationPass<mlir::func::FuncOp>> {
+        private:
+            void runOnOperation() override;
+
+            StringRef getArgument() const final { return "affine-full-unroll"; }
+
+            StringRef getDescription() const final {
+                return "Fully unroll all affine loops";
+            }
+        };
+
+class AffineFullUnrollPassAsPatternRewrite
+        : public PassWrapper<AffineFullUnrollPassAsPatternRewrite,
+                OperationPass<mlir::func::FuncOp>> {
+private:
+    void runOnOperation() override;
+
+    StringRef getArgument() const final { return "affine-full-unroll-rewrite"; }
+
+    StringRef getDescription() const final {
+        return "Fully unroll all affine loops using pattern rewrite engine";
+    }
+};
+
+
+    } // namespace tutorial
+} // namespace mlir
